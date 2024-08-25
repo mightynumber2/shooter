5fec066ca8f24b2ba5886bc6690769fe77c21200
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-24 19:43:52 -0700

    Add Drone enemy, update player movement and level
    - Added Drone enemy script and scene.
    - Updated player.gd to use CharacterBody2D for movement.
    - Modified level.tscn to include Drone and billboard.
    - Removed obsolete logo.gd script.
    - Updated bed.tscn and added billboard.tscn.

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,15 +1,6 @@
 extends Node2D
 
 var test_array: Array[String] = ["Test", "Hello", "stuff"]
-
-func _ready() -> void:
-	$Logo.rotation_degrees = 90
 	
-func _process(delta: float) -> void:
-	$Logo.rotation_degrees += 60 * delta
-	
-	if $Logo.position.x > 1000:
-		$Logo.pos.x = 0
-
 func test_function():
 	print("this is a test function")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
 
 	# laser shooting input
 	if Input.is_action_pressed("primary action"):
