260e541c2e7d53382f1921018123e681768b4b31
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-24 21:07:16 -0700

    Add gate w/ area detection & player reload timers
    - Added gate with area detection (body enter/exit signals).
    - Updated level scene with new gate and collision shapes.
    - Enhanced player script with laser and grenade cooldowns.
    - Added timers for laser and grenade reloading in player scene.

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -4,3 +4,11 @@ var test_array: Array[String] = ["Test", "Hello", "stuff"]
 	
 func test_function():
 	print("this is a test function")
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	print("body has entered")
+
+
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	print("body has exited")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 func _process(_delta: float) -> void:
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -7,8 +10,19 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 
 	# laser shooting input
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print("shooting mah lazers")
+		can_laser = false
+		$LaserReloadTimer.start()
+		
 	# shooting grenade input
-	if Input.is_action_pressed("secondary action"):
-		print("shoot grenade")                    
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		print("shoot grenade")     
+		can_grenade = false      
+		$GrenadeReloadTimer.start()
+
+func _on_laser_reload_timer_timeout() -> void:
+	can_laser = true
+
+func _on_grenade_reload_timer_timeout() -> void:
+	can_grenade = true
