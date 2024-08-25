ac3a600d827db9e9d4dcc486c7250e7019864cb8
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-25 15:34:15 -0700

    Add direction-based projectile handling
    - Updated laser and grenade to handle direction.
    - Refactored player and level scripts for direction support.
    - Adjusted player rotation and marker positions.
    - Added physics material and speed to grenade.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser(pos)
-signal grenade
+signal laser(pos, direction)
+signal grenade(pos, direction)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -11,21 +11,24 @@ func _process(_delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
 	move_and_slide()
-
+	# rotate
+	look_at(get_global_mouse_position())
+	
 	# laser shooting input
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	if Input.is_action_pressed("primary action") and can_laser:
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$LaserReloadTimer.start()
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	# shooting grenade input
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		can_grenade = false      
 		$GrenadeReloadTimer.start()
 		var pos = $LaserStartPositions.get_children()[0].global_position
-		grenade.emit(pos)
+		grenade.emit(pos, player_direction)
 
 func _on_laser_reload_timer_timeout() -> void:
 	can_laser = true
