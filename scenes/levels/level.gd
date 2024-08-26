78c1ff997ec15c8fe3345e84722ec0adf120671c
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-25 22:33:15 -0700

    Add collision layers and masks; update scenes
    - Added 2D physics layer names in project.godot
    - Set collision layers and masks for various nodes (Drone, House, Bed, Billboard, Gate, Grenade, Laser)
    - Updated positions and properties in house and level scenes
    - Corrected player and projectile collision settings

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -4,7 +4,7 @@ var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 
 func _on_gate_player_entered_gate(body) -> void:
-	print("body has entered")
+	print("player has entered")
 	print(body)
 
 func _on_player_laser(pos, direction) -> void:
