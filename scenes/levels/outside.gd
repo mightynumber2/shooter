cb441c20ce48239bc4383c53dfd88dbd9c0aca11
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-27 23:58:59 -0700

    Add scene transition for inside, outside gate
    - Added `outside_level_scene` export variable to `inside.gd`.
    - Updated `_on_exit_gate_area_body_entered` to use `call_deferred` for scene change in `inside.gd`.
    - Added `change_scene` method in `outside.gd` for scene transitions.
    - Added `outside_level_scene` resource reference in `inside.tscn`.
    - Updated player position in `outside.tscn`.
    - Removed unused resources (Toilet, Crate) in `outside.tscn`.

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -4,3 +4,8 @@ extends LevelParent
 func _on_gate_player_entered_gate(_body: Variant) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.8)
+	#get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	call_deferred("change_scene", "res://scenes/levels/inside.tscn")
+	
+func change_scene(scene_path: String) -> void:
+	get_tree().change_scene_to_file(scene_path)
