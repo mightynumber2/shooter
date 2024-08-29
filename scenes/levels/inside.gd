9090265136271077dcfe61df3442bcde41040cdc
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-28 22:15:56 -0700

    Add fade-to-black transitions between levels
    - Added `TransitionLayer` for fade-to-black effect.
    - Created `change_scene()` function in `transition_layer.gd` to handle scene transitions w/ fade effect.
    - Updated `inside.gd` & `outside.gd` to use `TransitionLayer` for scene changes.
    - Adjusted `inside.tscn` to remove redundant scene reference.

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,8 +1,10 @@
 extends LevelParent
 
-@export var outside_level_scene: PackedScene
 
 func _on_exit_gate_area_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.8)
-	get_tree().call_deferred("change_scene_to_packed", outside_level_scene)
+	call_deferred("change_scene", "res://scenes/levels/outside.tscn")
+	
+func change_scene(scene_path: String) -> void:
+	TransitionLayer.change_scene(scene_path)
