07b9bed20514c7c362162ed992eb2bb3f62d5ab7
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-30 00:02:16 -0700

    Fix scout laser gun logic variable name
    - Renamed `left_gun_use` to `right_gun_use` in `scout.gd`.

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var player_nearby: bool = false
 var can_laser: bool = true
-var left_gun_use: bool = true
+var right_gun_use: bool = true
 
 signal laser(pos, direction)
 
@@ -10,8 +10,8 @@ func _process(_delta: float) -> void:
 	if player_nearby:
 		look_at(Globals.player_pos)
 		if can_laser:
-			var marker_node = $LaserSpawnPositions.get_child(left_gun_use)
-			left_gun_use = not left_gun_use
+			var marker_node = $LaserSpawnPositions.get_child(right_gun_use)
+			right_gun_use = not right_gun_use
 			var pos: Vector2 = marker_node.global_position
 			var direction: Vector2 = (Globals.player_pos - position).normalized()
 			laser.emit(pos, direction)
