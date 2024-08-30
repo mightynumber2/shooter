e093703c3e6439388c1a0d6ff9abdf913ee5bfd0
DVo <83097985+mightynumber2@users.noreply.github.com>
2024-08-29 19:24:46 -0700

    Complete inside level decor
    - Fixed interior/exterior camera zoom logic & set proper Z-ordering.
    - Added multiple texture resources for decals & infestation.
    - Introduced new packed scenes for objects like beds & toilets.
    - Configured various decals & scene nodes w/ transformations & properties.
    - Positioned & scaled infestation elements across the scene.
    - Set up interactive elements such as toilets & crates.

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -16,34 +16,17 @@ func _on_container_opened(pos, direction):
 	item.tween_movement()
 	$Items.call_deferred("add_child", item)
 
+
 func _on_player_laser(pos, direction) -> void:
 	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
+	
 	
 func _on_player_grenade(pos, direction) -> void:
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.SPEED
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
-
-
-func _on_house_player_entered() -> void:
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	#tween.tween_property($Player, "modulate:a", 0, 1).from(0.5)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.7, 0.7), 1).set_trans(tween.TRANS_QUAD)
-
-
-func _on_house_player_exited() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.5, 0.5), 2)
-
-
-func _on_player_update_stats() -> void:
-	$UI.update_laser_text()
-	$UI.update_grenade_text()
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -8,3 +8,14 @@ func _on_gate_player_entered_gate(_body: Variant) -> void:
 
 func change_scene(scene_path: String) -> void:
 	TransitionLayer.change_scene(scene_path)
+
+
+func _on_house_player_entered() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.7, 0.7), 1).set_trans(tween.TRANS_QUAD)
+
+
+func _on_house_player_exited() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.5, 0.5), 2)
+	
